f273c08a62019dd539ca37a212824da8acd944d8
gregsommerville <me@gregsommerville.com>
2019-02-09 09:09:40 -0500

    Better default values

--- Models/EngineParameters.cs
+++ Models/EngineParameters.cs
@@ -9,25 +9,25 @@ namespace BlackjackStrategy.Models
     public sealed class EngineParameters
     {
         [Description("Number of candidates per generation")]
-        public int PopulationSize { get; set; } = 700;
+        public int PopulationSize { get; set; } = 750;
+
+        [Description("How parents are selected for crossover")]
+        public SelectionStyle SelectionStyle { get; set; } = Models.SelectionStyle.Tourney;
 
         [Description("If using Tourney Selection, how many to select")]
-        public int TourneySize { get; set; } = 4;
+        public int TourneySize { get; set; } = 3;
 
         [Description("Min number of generations it must run")]
         public int MinGenerations { get; set; } = 50;
 
         [Description("Max number of generations it can run")]
-        public int MaxGenerations { get; set; } = 200;
+        public int MaxGenerations { get; set; } = 500;
 
         [Description("Execution stops after this # gens with no improvement in best or average scores")]
-        public int MaxStagnantGenerations { get; set; } = 20;
-
-        [Description("How parents are selected for crossover")]
-        public SelectionStyle SelectionStyle { get; set; } = Models.SelectionStyle.Roulette;  
+        public int MaxStagnantGenerations { get; set; } = 25;
 
         [Description("From 0.0 to 1.0, percentage of the best scoring candidates moved to the next generation")]
-        public double ElitismRate { get; set; } = 0.15;
+        public double ElitismRate { get; set; } = 0;
 
         [Description("From 0.0 to 1.0, percentage of candidates that are mutated")]
         public double MutationRate { get; set; } = 0;
